de308a8f5b992b847aeb07b2a1a0d83d57c7ac1e
admin <admin>
2024-01-10 22:09:56 +0800

    增加播放, 暂停, 删除

--- src/components/main/music/layout/Play.vue
+++ src/components/main/music/layout/Play.vue
@@ -7,16 +7,16 @@
         <div class="play-content">
             <div class="play-info">
                 <div class="play-cover">
-                    <img src="/musicbg.png" alt="" onerror="this.src='/musicbg.png'"/>
+                    <img :src="playStore.currPlayMusic.img||'/musicbg.png'" alt="" onerror="this.src='/musicbg.png'"/>
                 </div>
                 <div class="info">
                     <div class="info-name">
-                        <span>听点什么呢</span>
+                        <span>{{ playStore.currPlayMusic.name||'听点什么呢' }}</span>
                     </div>
                     <div class="info-time">
                         <span class="current-time">00:00</span>
                         <span>/</span>
-                        <span class="total-time">00:00</span>
+                        <span class="total-time">{{ playStore.currPlayMusic.time||'00:00' }}</span>
                     </div>
                 </div>
             </div>
@@ -25,8 +25,8 @@
                     <svg t="1703726748044" class="icon" id="prev-control" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="10931" width="25" height="25"><path d="M213.333333 512a52.92 52.92 0 0 0 25.78 45.666667l618.666667 373.28a53.333333 53.333333 0 0 0 80.886667-45.666667V138.72a53.333333 53.333333 0 0 0-80.886667-45.666667L239.133333 466.333333A52.92 52.92 0 0 0 213.333333 512z m-128 405.333333V106.666667a21.333333 21.333333 0 0 1 42.666667 0v810.666666a21.333333 21.333333 0 0 1-42.666667 0z" fill="#5C5C66" p-id="10932"></path></svg>
                 </div>
                 <div>
-                    <svg t="1703727003306" class="icon" id="play-control" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="11303" width="25" height="25"><path d="M512 42.666667C252.793333 42.666667 42.666667 252.793333 42.666667 512s210.126667 469.333333 469.333333 469.333333 469.333333-210.126667 469.333333-469.333333S771.206667 42.666667 512 42.666667z m196.546667 500.493333l-266.666667 176A37.333333 37.333333 0 0 1 384 688V336.033333a37.333333 37.333333 0 0 1 57.893333-31.16l266.666667 176a37.333333 37.333333 0 0 1 0 62.32z" fill="#5C5C66" p-id="11304"></path></svg>
-                    <svg t="1703727089514" class="icon" id="pause-control" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="11489" width="25" height="25"><path d="M512 42.666667C252.793333 42.666667 42.666667 252.793333 42.666667 512s210.126667 469.333333 469.333333 469.333333 469.333333-210.126667 469.333333-469.333333S771.206667 42.666667 512 42.666667z m106.666667 682.666666a21.333333 21.333333 0 0 1-21.333334-21.333333V320a21.333333 21.333333 0 0 1 42.666667 0v384a21.333333 21.333333 0 0 1-21.333333 21.333333z m-213.333334 0a21.333333 21.333333 0 0 1-21.333333-21.333333V320a21.333333 21.333333 0 0 1 42.666667 0v384a21.333333 21.333333 0 0 1-21.333334 21.333333z" fill="#5C5C66" p-id="11490"></path></svg>
+                    <svg t="1703727003306" class="icon" @click="continueSong" :class="{play:playState}" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="11303" width="25" height="25"><path d="M512 42.666667C252.793333 42.666667 42.666667 252.793333 42.666667 512s210.126667 469.333333 469.333333 469.333333 469.333333-210.126667 469.333333-469.333333S771.206667 42.666667 512 42.666667z m196.546667 500.493333l-266.666667 176A37.333333 37.333333 0 0 1 384 688V336.033333a37.333333 37.333333 0 0 1 57.893333-31.16l266.666667 176a37.333333 37.333333 0 0 1 0 62.32z" fill="#5C5C66" p-id="11304"></path></svg>
+                    <svg t="1703727089514" class="icon" @click="pauseCurrentSong()" :class="{pause:!playState}" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="11489" width="25" height="25"><path d="M512 42.666667C252.793333 42.666667 42.666667 252.793333 42.666667 512s210.126667 469.333333 469.333333 469.333333 469.333333-210.126667 469.333333-469.333333S771.206667 42.666667 512 42.666667z m106.666667 682.666666a21.333333 21.333333 0 0 1-21.333334-21.333333V320a21.333333 21.333333 0 0 1 42.666667 0v384a21.333333 21.333333 0 0 1-21.333333 21.333333z m-213.333334 0a21.333333 21.333333 0 0 1-21.333333-21.333333V320a21.333333 21.333333 0 0 1 42.666667 0v384a21.333333 21.333333 0 0 1-21.333334 21.333333z" fill="#5C5C66" p-id="11490"></path></svg>
                 </div>
                 <div>
                     <svg t="1703726970407" class="icon" id="next-control" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="11117" width="25" height="25"><path d="M810.666667 512a52.92 52.92 0 0 1-25.78 45.666667l-618.666667 373.28a53.333333 53.333333 0 0 1-80.886667-45.666667V138.72a53.333333 53.333333 0 0 1 80.886667-45.666667l618.666667 373.28A52.92 52.92 0 0 1 810.666667 512z m128 405.333333V106.666667a21.333333 21.333333 0 0 0-42.666667 0v810.666666a21.333333 21.333333 0 0 0 42.666667 0z" fill="#5C5C66" p-id="11118"></path></svg>
@@ -55,7 +55,7 @@
                         <svg t="1704192055149" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3212" width="21" height="21"><path d="M978.56 224H379.52a32 32 0 0 1-32-32 32.64 32.64 0 0 1 32-32h599.04a32 32 0 0 1 32 32 32 32 0 0 1-32 32zM978.56 565.12H379.52a32 32 0 0 1-32-32 32.64 32.64 0 0 1 32-32h599.04a32 32 0 0 1 32 32 32 32 0 0 1-32 32zM978.56 906.88H379.52a32.64 32.64 0 0 1-32-32 32 32 0 0 1 32-32h599.04a32 32 0 0 1 32 32 32 32 0 0 1-32 32zM114.56 284.16a32.64 32.64 0 0 1-23.04-9.6L22.4 206.08a32.64 32.64 0 0 1 0-45.44 32 32 0 0 1 45.44 0l46.72 46.08 97.28-97.28a32 32 0 0 1 45.44 0 32.64 32.64 0 0 1 0 45.44L136.96 274.56a32 32 0 0 1-22.4 9.6zM114.56 618.88a32.64 32.64 0 0 1-23.04-9.6L22.4 540.16a32 32 0 0 1 0-44.8 32 32 0 0 1 45.44 0l46.72 46.08 97.28-97.28a32 32 0 0 1 45.44 0 32 32 0 0 1 0 44.8L136.96 609.28a32 32 0 0 1-22.4 9.6zM114.56 967.04a32.64 32.64 0 0 1-23.04-7.04L22.4 888.32a32.64 32.64 0 0 1 0-45.44 32.64 32.64 0 0 1 45.44 0l46.72 46.72 97.28-97.92a32 32 0 1 1 45.44 45.44L136.96 960a32 32 0 0 1-22.4 7.04z" fill="#323333" p-id="3213"></path></svg>
                     </div>
                     <div title="清空">
-                        <svg t="1704192032029" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2964" width="21" height="21"><path d="M782.72 1014.4H233.6a92.8 92.8 0 0 1-83.84-101.12V288.64a32 32 0 1 1 64 0v624.64c0 23.04 12.16 37.76 20.48 37.76h549.12c8.32 0 20.48-14.72 20.48-37.76V288.64a32 32 0 1 1 64 0v624.64a92.8 92.8 0 0 1-85.12 101.12zM960 186.24H57.6a31.36 31.36 0 0 1-32-31.36 32 32 0 0 1 32-32H960a32 32 0 0 1 32 32 31.36 31.36 0 0 1-32 31.36zM665.6 72.96H350.72a32 32 0 1 1 0-64h314.88a32 32 0 1 1 0 64z" fill="#323333" p-id="2965"></path><path d="M407.04 873.6a31.36 31.36 0 0 1-31.36-31.36V341.76a32 32 0 1 1 64 0v500.48a31.36 31.36 0 0 1-32.64 31.36zM609.28 873.6a31.36 31.36 0 0 1-32-31.36V341.76a32 32 0 1 1 64 0v500.48a31.36 31.36 0 0 1-32 31.36z" fill="#323333" p-id="2966"></path></svg>
+                        <svg t="1704192032029" @click="playStore.clearQueue" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2964" width="21" height="21"><path d="M782.72 1014.4H233.6a92.8 92.8 0 0 1-83.84-101.12V288.64a32 32 0 1 1 64 0v624.64c0 23.04 12.16 37.76 20.48 37.76h549.12c8.32 0 20.48-14.72 20.48-37.76V288.64a32 32 0 1 1 64 0v624.64a92.8 92.8 0 0 1-85.12 101.12zM960 186.24H57.6a31.36 31.36 0 0 1-32-31.36 32 32 0 0 1 32-32H960a32 32 0 0 1 32 32 31.36 31.36 0 0 1-32 31.36zM665.6 72.96H350.72a32 32 0 1 1 0-64h314.88a32 32 0 1 1 0 64z" fill="#323333" p-id="2965"></path><path d="M407.04 873.6a31.36 31.36 0 0 1-31.36-31.36V341.76a32 32 0 1 1 64 0v500.48a31.36 31.36 0 0 1-32.64 31.36zM609.28 873.6a31.36 31.36 0 0 1-32-31.36V341.76a32 32 0 1 1 64 0v500.48a31.36 31.36 0 0 1-32 31.36z" fill="#323333" p-id="2966"></path></svg>
                     </div>
                 </div>
             </div>
@@ -67,16 +67,16 @@
                             <div class="singer">
                                 <span v-for="(singer, i) in item.singer">
                                     {{ singer.name }}
-                                    <span v-if="i < item.singer.length - 1">, </span>  
+                                    <span v-if="i < item.singer.length - 1">, </span>
                                 </span>
                             </div>
                             <span class="time">{{ item.time }}</span>
                         </div>
                     </div>
                     <div class="control">
-                        <svg t="1703991609601" class="icon play" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5474" width="21" height="21"><path d="M870.2 466.333333l-618.666667-373.28a53.333333 53.333333 0 0 0-80.866666 45.666667v746.56a53.206667 53.206667 0 0 0 80.886666 45.666667l618.666667-373.28a53.333333 53.333333 0 0 0 0-91.333334z" fill="#5C5C66" p-id="5475"></path></svg>
-                        <svg t="1703991646436" class="icon pause" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5660" width="21" height="21"><path d="M426.666667 138.666667v746.666666a53.393333 53.393333 0 0 1-53.333334 53.333334H266.666667a53.393333 53.393333 0 0 1-53.333334-53.333334V138.666667a53.393333 53.393333 0 0 1 53.333334-53.333334h106.666666a53.393333 53.393333 0 0 1 53.333334 53.333334z m330.666666-53.333334H650.666667a53.393333 53.393333 0 0 0-53.333334 53.333334v746.666666a53.393333 53.393333 0 0 0 53.333334 53.333334h106.666666a53.393333 53.393333 0 0 0 53.333334-53.333334V138.666667a53.393333 53.393333 0 0 0-53.333334-53.333334z" fill="#5C5C66" p-id="5661"></path></svg>
-                        <svg t="1704248260169" class="icon del" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4450" width="21" height="21"><path d="M412 744c-19.8 0-36-16.2-36-36V500c0-19.8 16.2-36 36-36s36 16.2 36 36v208c0 19.8-16.2 36-36 36zM612 744c-19.8 0-36-16.2-36-36V500c0-19.8 16.2-36 36-36s36 16.2 36 36v208c0 19.8-16.2 36-36 36z" fill="#333333" p-id="4451"></path><path d="M923.8 248H736v-82c0-56.2-45.8-102-102-102H390c-56.2 0-102 45.8-102 102v82H100.2C80.3 248 64 264.2 64 284s16.3 36 36.2 36H192v518c0 16.4 3.2 32.4 9.6 47.5 6.1 14.5 14.9 27.6 26.1 38.8 11.2 11.2 24.2 20 38.8 26.1 15.1 6.4 31.1 9.6 47.5 9.6h396c16.4 0 32.4-3.2 47.5-9.6 14.5-6.1 27.6-14.9 38.8-26.1 11.2-11.2 20-24.2 26.1-38.8 6.4-15.1 9.6-31.1 9.6-47.5V320h91.8c19.9 0 36.2-16.2 36.2-36s-16.3-36-36.2-36zM360 166c0-16.6 13.4-30 30-30h244c16.6 0 30 13.4 30 30v82H360v-82z m400 672c0 27.6-22.4 50-50 50H314c-27.6 0-50-22.4-50-50V320h496v518z" fill="#333333" p-id="4452"></path></svg>
+                        <svg t="1703991609601" class="icon" @click="playCurrentSong(item)" :class="{playHide:item.mid==playStore.currPlayMusic.mid&&playState}" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5474" width="18" height="18"><path d="M870.2 466.333333l-618.666667-373.28a53.333333 53.333333 0 0 0-80.866666 45.666667v746.56a53.206667 53.206667 0 0 0 80.886666 45.666667l618.666667-373.28a53.333333 53.333333 0 0 0 0-91.333334z" fill="#5C5C66" p-id="5475"></path></svg>
+                        <svg t="1703991646436" class="icon" @click="pauseCurrentSong()" :class="{playHide:!(item.mid==playStore.currPlayMusic.mid&&playState)}" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5660" width="18" height="18"><path d="M426.666667 138.666667v746.666666a53.393333 53.393333 0 0 1-53.333334 53.333334H266.666667a53.393333 53.393333 0 0 1-53.333334-53.333334V138.666667a53.393333 53.393333 0 0 1 53.333334-53.333334h106.666666a53.393333 53.393333 0 0 1 53.333334 53.333334z m330.666666-53.333334H650.666667a53.393333 53.393333 0 0 0-53.333334 53.333334v746.666666a53.393333 53.393333 0 0 0 53.333334 53.333334h106.666666a53.393333 53.393333 0 0 0 53.333334-53.333334V138.666667a53.393333 53.393333 0 0 0-53.333334-53.333334z" fill="#5C5C66" p-id="5661"></path></svg>
+                        <svg t="1704248260169" class="icon del" @click="playStore.removeMusic(item)" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4450" width="18" height="18"><path d="M412 744c-19.8 0-36-16.2-36-36V500c0-19.8 16.2-36 36-36s36 16.2 36 36v208c0 19.8-16.2 36-36 36zM612 744c-19.8 0-36-16.2-36-36V500c0-19.8 16.2-36 36-36s36 16.2 36 36v208c0 19.8-16.2 36-36 36z" fill="#333333" p-id="4451"></path><path d="M923.8 248H736v-82c0-56.2-45.8-102-102-102H390c-56.2 0-102 45.8-102 102v82H100.2C80.3 248 64 264.2 64 284s16.3 36 36.2 36H192v518c0 16.4 3.2 32.4 9.6 47.5 6.1 14.5 14.9 27.6 26.1 38.8 11.2 11.2 24.2 20 38.8 26.1 15.1 6.4 31.1 9.6 47.5 9.6h396c16.4 0 32.4-3.2 47.5-9.6 14.5-6.1 27.6-14.9 38.8-26.1 11.2-11.2 20-24.2 26.1-38.8 6.4-15.1 9.6-31.1 9.6-47.5V320h91.8c19.9 0 36.2-16.2 36.2-36s-16.3-36-36.2-36zM360 166c0-16.6 13.4-30 30-30h244c16.6 0 30 13.4 30 30v82H360v-82z m400 672c0 27.6-22.4 50-50 50H314c-27.6 0-50-22.4-50-50V320h496v518z" fill="#333333" p-id="4452"></path></svg>
                     </div>
                 </li>
             </ul>
@@ -87,16 +87,61 @@
                 </div>
             </div>
         </div>
-        <audio></audio>
+        <audio :src="playStore.currPlayMusic.playUrl" @timeupdate="timeupdate"></audio>
     </div>
 </template>
 
 <script  lang="ts" setup>
 import playMusic from "@/store/modules/playMusic";
 import { ref } from "vue";
-let playStore = playMusic();
+import { eventBus } from "@/common/eventBus";
+import { formatTime } from '@/common/utils'
+import { music } from "@/type/musicTypes";
 
+let playStore = playMusic();
 const playQueueOpen = ref(false)
+const playState = ref(false)
+
+const timeupdate = () => {
+    let audio = document.getElementsByTagName("audio")[0] as HTMLAudioElement;
+    let currentTime = document.getElementsByClassName("current-time")[0] as HTMLElement;
+    currentTime.textContent = formatTime(audio.currentTime)
+};
+
+const continueSong = () => {
+    if (playStore.currPlayMusic.playUrl) {
+        let audio = document.getElementsByTagName("audio")[0] as HTMLAudioElement;
+        audio.play();
+        playState.value = true;
+        eventBus.emit("get-play-state", playState.value);
+    }
+}
+
+const playCurrentSong = async (music: music) => {
+    playStore.addMusic(music)
+    await playStore.getSongDetail(music);
+    if (playStore.currPlayMusic.playUrl) {
+        let audio = document.getElementsByTagName("audio")[0] as HTMLAudioElement;
+        audio.play();
+        playState.value = true;
+        eventBus.emit("get-play-state", playState.value);
+    }
+}
+
+const pauseCurrentSong = async () => {
+    let audio = document.getElementsByTagName("audio")[0] as HTMLAudioElement;
+    audio.pause();
+    playState.value = false;
+    eventBus.emit("get-play-state", playState.value);
+}
+
+eventBus.on("pause-music", pauseCurrentSong);
+
+eventBus.on("play-music", (data: any) => {
+    playCurrentSong(data);
+});
+
+
 
 const closeQueue = () => {
     playQueueOpen.value = false;
@@ -178,7 +223,10 @@ document.addEventListener("click", closeQueue)
         div:hover path {
             fill: var(--control-btn-color);
         }
-        #pause-control {
+        .play {
+            display: none;
+        }
+        .pause {
             display: none;
         }
     }
@@ -305,9 +353,9 @@ document.addEventListener("click", closeQueue)
                 display: flex;
                 align-items: center;
                 justify-content: space-between;
-                width: 60px;
+                width: 50px;
                 display: none;
-                .pause {
+                .playHide {
                     display: none;
                 }
                 svg {
@@ -323,6 +371,9 @@ document.addEventListener("click", closeQueue)
         }
         li:hover .control{
             display: block;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
         }
         li:hover .info-line .time{
             display: none;
