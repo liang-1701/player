630d55b2f83fd0f4e20a5e45cc51005c2d21d720
admin <admin>
2024-01-23 10:34:50 +0800

    优化一下

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import "element-plus/dist/index.css";
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import ElementPlus from "element-plus";
 import zhCn from "element-plus/es/locale/lang/zh-cn.mjs";
-import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 import { createPinia } from 'pinia'
 import router from './router'
 import VueLazyLoad from "vue3-lazyload";
@@ -16,9 +15,6 @@ import '@/common/request'
 const app = createApp(App);
 // app.config.globalProperties.$global = global
 app.use(ElementPlus, { locale: zhCn });
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component);
-}
 app.use(createPinia());
 app.use(router);
 app.use(VueLazyLoad, {
