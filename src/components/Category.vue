6ee812643872a74429957614f1e6918c5560636d
liang <liang571701@163.com>
2024-06-05 20:27:12 +0800

    提取组件

---
+++ src/components/Category.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- 歌单展示 -->
+    <div class="Category">
+        ceshi
+8/10000
+实时翻译
+category
+英 [ˈkætəɡəri]美 [ˈkætəɡɔːri]
+n.类别; (人或事物的)种类
+划译
+详细释义
+类别
+    </div>
+</template>
+
+<script  lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
